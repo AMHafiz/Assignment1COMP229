02f4d850a43a893e42cbb2b79ffce8192e291e18
Aisha Abdulhafiz <ayshag702@gmail.com>
2024-10-11 10:13:26 -0400

    Mongoose Changes

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   created: { type: Date, default: Date.now },
-  updated: { type: Date, default: Date.now },
+  updated: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
